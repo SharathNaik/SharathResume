c31afde529318da52d471eafbcf43a5740affdce
SharathNaik <nayak.sharathm@gmail.com>
2019-08-21 22:42:50 +0530

    Netlify Updations made

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import 'react-mdl/extra/material.js';
 
 
 ReactDOM.render(
-  <BrowserRouter basename={'/SharathResume'}>
+  <BrowserRouter>
   <App />
   </BrowserRouter>,
   document.getElementById('root')
