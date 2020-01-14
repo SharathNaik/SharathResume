d3b2a99b3c21598477e839b167e9346b5072556e
SharathNaik <nayak.sharathm@gmail.com>
2020-01-14 10:26:08 +0530

    Fixed About Me page and Left Navigation Hide after click.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,68 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 
-import Main from './components/main';
-import {Link} from 'react-router-dom';
+import Main from "./components/main";
+import { Link } from "react-router-dom";
 
 class App extends Component {
+  //Fix onclick Toggle close
+  hideToggle() {
+    var selectorId = document.querySelector(".mdl-layout");
+    selectorId.MaterialLayout.toggleDrawer();
+  }
+
   render() {
     return (
       <div className="demo-big-content">
         <Layout>
-            <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
-                <Navigation>
-                    <Link to="/resume">Resume</Link>
-                    <Link to="/aboutme">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contact">Contact</Link>
-                </Navigation>
-            </Header>
-            <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
-                <Navigation>
-                    <Link to="/resume">Resume</Link>
-                    <Link to="/aboutme">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contact">Contact</Link>
-                </Navigation>
-            </Drawer>
-            <Content>
-                <div className="page-content" />
-                <Main />
-            </Content>
+          <Header
+            className="header-color"
+            title={
+              <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                MyPortfolio
+              </Link>
+            }
+            scroll
+          >
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer
+            title={
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to="/"
+                onClick={() => this.hideToggle()}
+              >
+                MyPortfolio
+              </Link>
+            }
+          >
+            <Navigation>
+              <Link to="/resume" onClick={() => this.hideToggle()}>
+                Resume
+              </Link>
+              <Link to="/aboutme" onClick={() => this.hideToggle()}>
+                About Me
+              </Link>
+              <Link to="/projects" onClick={() => this.hideToggle()}>
+                Projects
+              </Link>
+              <Link to="/contact" onClick={() => this.hideToggle()}>
+                Contact
+              </Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
         </Layout>
       </div>
     );
--- src/components/customBanners.js
+++ src/components/customBanners.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CustomBanners extends Component {
   render() {
-    return(
-        <div className={"item knowledge "+this.props.dataTitle} data-title={this.props.dataTitle} data-year={this.props.dataYear} style={{backgroundColor:(this.props.dataColor)}}>
-            <div className="item-content">
-            <div className="header">{this.props.dataHeader}</div>
-            <div className="year">{this.props.dataYear}</div>
-            </div>
+    return (
+      <div
+        className={"item knowledge " + this.props.dataTitle}
+        data-header={this.props.dataHeader}
+        data-title={this.props.dataTitle}
+        data-year={this.props.dataYear}
+        style={{ backgroundColor: this.props.dataColor }}
+      >
+        <div className="item-content">
+          <div className="header">{this.props.dataHeader}</div>
+          <div className="year">{this.props.dataYear}</div>
         </div>
-    )
+      </div>
+    );
   }
 }
 
